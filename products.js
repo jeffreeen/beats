7452e925f74a8c86d00cadbdb23bf730d1ff228f
jeffreeen <96103981+jeffreeen@users.noreply.github.com>
2022-03-27 10:30:39 +0300

    111

---
+++ products.js
@@ -0,0 +1,15 @@
+const slider = $('.slider__list').bxSlider({
+  pager:false,
+  controls: false
+});
+
+$(".slider__button-left").click(e => {
+  e.preventDefault();
+
+  slider.goToPrevSlide();
+})
+$(".slider__button-right").click(e=> {
+  e.preventDefault();
+
+  slider.goToNextSlide();
+})
